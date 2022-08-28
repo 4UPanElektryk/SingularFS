5254db5333c8ef9c6abe7bf6de783e9cb1d7bde7
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-08-28 18:41:42 +0200

    Initial commit

---
+++ FileFS/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FileFS
+{
+    public class FileFSFile
+    {
+        public string Name;
+        public string Content;
+        public int Length;
+        public static FileFSFile Create(string name,string[] content)
+        {
+            FileFSFile f = new FileFSFile
+            {
+                Name = name,
+                Content = string.Join("\r\n", content),
+                Length = string.Join("\r\n", content).Length,
+            };
+            return f;
+        }
+    }
+}
